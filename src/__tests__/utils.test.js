c95d4e606728ddb8c6c7798b9c92b48a71a504ff
Monica Garcia <monicairenegarcia@gmail.com>
2024-02-19 09:34:01 -0700

    add test for empty string

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,11 +1,18 @@
 import { isPalindrome } from "../utils.js";
 
 describe("isPalindrome", () => {
-    it("should return 'true' if function is palindrome, otherwise return false", () => {
+    it("should return 'true' if word is palindrome, otherwise return false", () => {
         const palindrome = "racecar";
 
         const isPalindromeTest = isPalindrome(palindrome);
 
         expect(isPalindromeTest).toBe(true)
+    })
+    it("should return 'false' if word is empty string", () => {
+        const emptyString = "";
+
+        const isPalindromeTestEmptyString = isPalindrome(emptyString);
+
+        expect(isPalindromeTestEmptyString).toBe(false);
     })
 })
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,10 @@ export function isPalindrome(word) {
     const wordArray = word.split('');
     let palindromeCheck = false;
     for (let i=0; i<wordArray.length; i++) {
-        if (wordArray[i].toLowerCase() === wordArray[wordArray.length - i - 1].toLowerCase()) {
+        if (word === "") {
+            palindromeCheck = false
+        }
+        else if (wordArray[i].toLowerCase() === wordArray[wordArray.length - i - 1].toLowerCase()) {
             palindromeCheck = true;
             console.log("i: " + i + ", " + wordArray.length - i)
         }
